938fe954bf8f2137d544583eb7559f5a04936122
IsaacFerguson <tul24458@temple.edu>
2024-05-07 19:04:30 -0400

    display noise filter

---
+++ islandGen/Assets/Scripts/Noise.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+   public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float persistance, float lacunarity){
+    float[,] noiseMap = new float[mapWidth, mapHeight];
+
+    if (scale <= 0){
+        scale = 0.0001f;
+    }
+
+    float maxNoiseHeight = float.MinValue;
+    float minNoiseHeight = float.MinValue;
+
+    for (int y = 0; y < mapHeight; y++){
+        for (int x = 0; x < mapWidth; x++){
+
+            float amplutide = 1;
+            float frequency = 1;
+            float noiseHeight = 0;
+
+
+            for( int i = 0; i < octaves; i++){
+                float sampleX = x / scale * frequency;
+                float sampleY = y / scale * frequency;
+
+                float perlinValue = Mathf.PerlinNoise (sampleX, sampleY) * 2 - 1;
+                noiseHeight += perlinValue * amplutide;
+
+                amplutide *= persistance;
+                frequency *= lacunarity;
+            }
+            
+            if (noiseHeight > maxNoiseHeight){
+                maxNoiseHeight = noiseHeight;
+            } else if (noiseHeight < minNoiseHeight){
+                minNoiseHeight = noiseHeight;
+            }
+
+
+            noiseMap [x, y] = noiseHeight;
+        }
+    }
+
+        for (int y = 0; y < mapHeight; y++){
+            for (int x = 0; x < mapWidth; x++){
+                noiseMap [x,y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap [x,y]);
+            }
+        }
+
+        return noiseMap;
+   }
+}
